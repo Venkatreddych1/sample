c1f0d8c4914c3eec5a96e94135f9529f784a52ed
venkat <chereddyvenkatareddy@gmail.com>
2019-05-15 09:01:18 +0530

    firstcommit

--- infibeam2/settings.py
+++ infibeam2/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'infibeam2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'venki',
+        'USER':'root',
+        'PASSWORD':'venki1234',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
